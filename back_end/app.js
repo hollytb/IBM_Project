4e2c9acadd60585fe6c31a4081c69b623a9ebf93
baker ho <bakerho@tcd.ie>
2020-02-27 15:07:35 +0000

    Connect front-end to back-end

--- back_end/app.js
+++ back_end/app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var cors = require('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 testBackEnd = require("./routes/test_back_end"); 
@@ -15,6 +15,7 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors()); // CORS: allows to skip the same origin policy + access resources from remote hosts 
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
--- client2/src/App.js
+++ client2/src/App.js
@@ -2,26 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={apiResponse:""}
+  }
+  // Comunicate with API
+  callAPI(){
+    fetch("http://localhost:9000/test_back_end") //React app talks to API at this url
+    .then(res=>res.text())
+    .then(res=>this.setState({apiResponse: res}));
+  }
+  componentWillMount(){
+    this.callAPI();
+  }
 
-function App() {
+
+render() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{this.state.apiResponse}</p>// print response from api
       </header>
     </div>
   );
 }
-
+}
 export default App;
