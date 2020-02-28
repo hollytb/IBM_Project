806dd8704b713eaa228e36843a6b829acf444b5e
Nissimol Aji <44730306+nissimanjayil@users.noreply.github.com>
2020-02-28 15:21:55 +0000

    Adding the python files

---
+++ back_end/sample-client.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+x = json.load(open('sample-request.json', 'rt'))
+headers = {'Content-Type': 'application/json'}
+
+
+r = requests.post('http://theia.eu-gb.mybluemix.net/classify',
+                  data=json.dumps(x), headers=headers)
+'''
+if r.status_code == 200:
+    print(r.json())
+else:
+    print(r.text)
+'''
+JsonDictionary = {}
+for i, d in enumerate(x):
+    JsonDictionary[f"dict{i}"] = d
+print(JsonDictionary['dict3']['roi_number'])
