c84c64d0bd1d3f62a8742bcd50b6b54f53e3d08a
Nissi <nissimanjayil@gmail.com>
2020-03-12 22:43:02 +0000

    Changed the CallAPI fectch URL

--- client2/src/App.js
+++ client2/src/App.js
@@ -1,45 +1,44 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react';
+import "bootstrap/dist/css/bootstrap.css";
+import React from "react";
 
-import Buttons_Footer from './components/Buttons_Footer.js';
-import LeftPane from './components/LeftPane.js';
+import Buttons_Footer from "./components/Buttons_Footer.js";
+import LeftPane from "./components/LeftPane.js";
 
 //import './App.css';
 
-class App extends React.Component{
-  constructor(props){
+class App extends React.Component {
+  constructor(props) {
     super(props);
-    this.state={apiResponse:""}
+    this.state = { apiResponse: "" };
     this.state = {
-      number:[1,2,3,4,5,6,7,]
-    }
+      number: [1, 2, 3, 4, 5, 6, 7]
+    };
   }
   // Comunicate with API
-  callAPI(){
-    fetch("http://localhost:9000/test_back_end") //React app talks to API at this url
-    .then(res=>res.text())
-    .then(res=>this.setState({apiResponse: res}));
+  callAPI() {
+    fetch("http://localhost:9000/IntensityAPI") //React app talks to API at this url
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
   }
-  componentWillMount(){
+  componentWillMount() {
     this.callAPI();
   }
-render() {
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>{this.state.apiResponse}</p>
-        <div class="row fixed-bottom no-gutters">
-          <div class="col-3 fixed-top fixed-bottom">
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>{this.state.apiResponse}</p>
+          <div class="row fixed-bottom no-gutters">
+            <div class="col-3 fixed-top fixed-bottom">
               <LeftPane></LeftPane>
-          </div>
-          <div class="offset-md-3" >
+            </div>
+            <div class="offset-md-3">
               <Buttons_Footer></Buttons_Footer>
+            </div>
           </div>
-       </div>
-      </header>
-    </div>
-  );
-}
+        </header>
+      </div>
+    );
+  }
 }
 export default App;
