244545919e9780feff567eea7988bcd6242cc0c5
Nissi <nissimanjayil@gmail.com>
2020-03-12 22:34:11 +0000

    Displays Sample-request.JSON in API and Need to implement Post Method

--- back_end/app.js
+++ back_end/app.js
@@ -4,9 +4,8 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var cors = require("cors");
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-testBackEndRouter = require("./routes/testBackEnd");
+
+var testBackEndRouter = require("./routes/testBackEnd");
 var app = express();
 
 // view engine setup
@@ -20,8 +19,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/testBackEnd", testBackEndRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- back_end/routes/testBackEnd.js
+++ back_end/routes/testBackEnd.js
@@ -1,10 +1,36 @@
 var express = require("express");
 var router = express.Router();
-//const data = require("./back_end/Tracker/Output.json");
+//Path to the JSON file when created
+const jsonFilePath = "./Tracker/sample-request.json";
+const fs = require("fs");
 
-router.get("/search", function(req, res) {
-  res.send("API IS WORKING....");
-  //res.json(data);
+//This method reads the file
+const readFile = (
+  callback,
+  returnJson = false,
+  filePath = jsonFilePath,
+  encoding = "utf8"
+) => {
+  fs.readFile(filePath, encoding, (err, data) => {
+    if (err) {
+      throw err;
+    }
+
+    callback(returnJson ? JSON.parse(data) : data);
+  });
+};
+
+//Sends the JSON data,Parses this data and Displays
+router.get("/", function(req, res) {
+  fs.readFile(jsonFilePath, "utf8", (err, data) => {
+    if (err) {
+      throw err;
+    }
+
+    res.send(JSON.parse(data));
+  });
 });
+
+//Make a method POST API
 
 module.exports = router;
