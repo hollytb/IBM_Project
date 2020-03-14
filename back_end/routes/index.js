621acdc95aa464023754e28e6c4de5c0a740a074
Liam Bartsch <bartschl@tcd.ie>
2020-03-14 18:06:05 +0000

    testing for running vanilla_tracker.py from node.js

--- back_end/routes/index.js
+++ back_end/routes/index.js
@@ -1,3 +1,8 @@
-const path = require("path");
+/*const path = require("path");
 const { spawn } = require("child_process");
-const pythonProcess = spawn("python", ["Tracker/vanilla_tracker.py", arg1]);
+const pythonProcess = spawn("python", ["Tracker/vanilla_tracker.py", arg1]);*/
+
+var express = require("express");
+var app = express();
+var spawn = require("child_process").spawn;
+var process = spawn("python", ["./Tracker/vanilla_tracker.py"]);
