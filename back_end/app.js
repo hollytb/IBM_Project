1daf810cf155b2c16d72de74e2b343eb41c479aa
Nissimol Aji <44730306+nissimanjayil@users.noreply.github.com>
2020-03-16 12:59:58 +0000

    Add files via upload

--- back_end/app.js
+++ back_end/app.js
@@ -6,7 +6,7 @@ var logger = require("morgan");
 var cors = require("cors");
 
 var IntensityAPIRouter = require("./routes/IntensityAPI");
-var indexAPIRouter = require("./routes/index");
+//var indexAPIRouter = require("./routes/index");
 var app = express();
 
 // view engine setup
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/IntensityAPI", IntensityAPIRouter);
-app.use("/", indexAPIRouter);
+//app.use("/", indexAPIRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
