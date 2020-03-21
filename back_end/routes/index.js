58db4ab961772137c46b22300767834ea8fcf307
Liam Bartsch <bartschl@tcd.ie>
2020-03-21 15:46:59 +0100

    Tried to connect node.js still not working

--- back_end/routes/index.js
+++ back_end/routes/index.js
@@ -1,5 +1,26 @@
-const spawn = require("child_process").spawn;
-const process = spawn("python", ["./vanilla_tracker.py"]);
-process.stdout.on("data", data => {
-  console.log(data.toString());
+const express = require("express");
+const app = express();
+
+let runPy = new Promise(function(success, nosuccess) {
+  const { spawn } = require("child_process");
+  const pyprog = spawn("python", ["./../vanilla_tracker.py"]);
+
+  pyprog.stdout.on("data", function(data) {
+    success(data);
+  });
+
+  pyprog.stderr.on("data", data => {
+    nosuccess(data);
+  });
 });
+
+app.get("/", (req, res) => {
+  res.write("welcome\n");
+
+  runPy.then(function(fromRunpy) {
+    console.log(fromRunpy.toString());
+    res.end(fromRunpy);
+  });
+});
+
+app.listen(4000, () => console.log("Application listening on port 4000!"));
