0537c11e18a9d5c5e23d168d182a5d3df75d900b
Liam Bartsch <bartschl@tcd.ie>
2020-03-21 15:53:32 +0100

    Fixed router error but still pythonshell error.

--- back_end/routes/index.js
+++ back_end/routes/index.js
@@ -1,26 +1,8 @@
+let { PythonShell } = require("python-shell");
 const express = require("express");
-const app = express();
-
-let runPy = new Promise(function(success, nosuccess) {
-  const { spawn } = require("child_process");
-  const pyprog = spawn("python", ["./../vanilla_tracker.py"]);
-
-  pyprog.stdout.on("data", function(data) {
-    success(data);
-  });
-
-  pyprog.stderr.on("data", data => {
-    nosuccess(data);
-  });
+const router = express.Router();
+module.exports = router;
+PythonShell.run("vanilla_tracker.py", function(err) {
+  if (err) throw err;
+  console.log("finished");
 });
-
-app.get("/", (req, res) => {
-  res.write("welcome\n");
-
-  runPy.then(function(fromRunpy) {
-    console.log(fromRunpy.toString());
-    res.end(fromRunpy);
-  });
-});
-
-app.listen(4000, () => console.log("Application listening on port 4000!"));
