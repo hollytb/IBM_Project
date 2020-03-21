a14ca41af1b47685d05d3bf11688c053dfc18f18
Liam Bartsch <bartschl@tcd.ie>
2020-03-21 19:15:10 +0100

    connected the python script but can't read video

--- back_end/routes/index.js
+++ back_end/routes/index.js
@@ -1,14 +1,8 @@
-var myPythonScriptPath = "vanilla_tracker.py";
-var PythonShell = require("python-shell");
-var pyshell = new PythonShell(myPythonScriptPath);
-pyshell.on("message", function(message) {
-  // received a message sent from the Python script (a simple "print" statement)
-  console.log(message);
-});
-pyshell.end(function(err) {
-  if (err) {
-    throw err;
-  }
-
+var myPythonScriptPath =
+  "/Users/liam/Desktop/TCD/SWENG/github/ibm_project/back_end/Tracker/vanilla_tracker.py";
+let { PythonShell } = require("python-shell");
+//var pyshell = new PythonShell(myPythonScriptPath, options);
+PythonShell.run(myPythonScriptPath, null, function(err) {
+  if (err) throw err;
   console.log("finished");
 });
