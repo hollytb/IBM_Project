080044390ca2ecd4b1156afec20b1d302323f4c2
Alan Abraham <47579004+Alantrivandrum@users.noreply.github.com>
2020-03-21 12:34:13 +0000

    Added video component

---
+++ client2/src/components/Video.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import ReactPlayer from 'react-player'
+
+class Video extends Component {
+    render () {
+      return (
+        <div className='player-wrapper'>
+          <ReactPlayer
+            className='react-player'
+            url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
+            width='100%'
+            height='100%'
+          />
+        </div>
+      )
+    }
+  }
+
+  export default Video;
