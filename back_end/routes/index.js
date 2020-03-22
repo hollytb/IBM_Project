364244ba540c8d293a2671349e58e12f7e485d3c
Liam Bartsch <bartschl@tcd.ie>
2020-03-22 09:04:10 +0100

    fixed errors but new can't find cv2 error

--- back_end/routes/index.js
+++ back_end/routes/index.js
@@ -1,8 +1,34 @@
-var myPythonScriptPath =
-  "/Users/liam/Desktop/TCD/SWENG/github/ibm_project/back_end/Tracker/vanilla_tracker.py";
-let { PythonShell } = require("python-shell");
-//var pyshell = new PythonShell(myPythonScriptPath, options);
-PythonShell.run(myPythonScriptPath, null, function(err) {
-  if (err) throw err;
-  console.log("finished");
+var express = require("express");
+var router = express.Router();
+var myPythonScriptPath = "../Tracker/vanilla_tracker.py";
+const fs = require("fs");
+module.exports = router;
+
+const path = require("path");
+const { spawn } = require("child_process");
+
+/**
+ * Run python script, pass in `-u` to not buffer console output
+ * @return {ChildProcess}
+ */
+function runScript() {
+  return spawn("python", [
+    "-u",
+    path.join(__dirname, myPythonScriptPath),
+    "--foo",
+    "some value for foo"
+  ]);
+}
+
+const subprocess = runScript();
+
+// print output of script
+subprocess.stdout.on("data", data => {
+  console.log(`data:${data}`);
+});
+subprocess.stderr.on("data", data => {
+  console.log(`error:${data}`);
+});
+subprocess.on("close", () => {
+  console.log("Closed");
 });
