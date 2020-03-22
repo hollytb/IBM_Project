d489d8eddd8865dfbd401aee9a5153b076793a50
Alan Abraham <47579004+Alantrivandrum@users.noreply.github.com>
2020-03-22 13:18:09 +0000

    Fixed video component to play local videos

--- client2/src/components/Video.js
+++ client2/src/components/Video.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
 import ReactPlayer from 'react-player'
+import myVideo from "C:/Users/alans/Desktop/ibm_project/client2/src/video.mp4"
+
+
+
 
 class Video extends Component {
     render () {
@@ -7,9 +11,11 @@ class Video extends Component {
         <div className='player-wrapper'>
           <ReactPlayer
             className='react-player fixed-bottom'
-            url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
+            url= {myVideo}
             width='100%'
-            height='100%'
+            height='110%'
+            controls = {true}
+             
           />
         </div>
       )
