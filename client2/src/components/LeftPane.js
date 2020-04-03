75a9298fc3f3a8bd80a41f937690773d96809096
mariaalonso2705 <mariaalonsogarcia@outlook.com>
2020-04-03 19:51:16 +0200

    Adjusted size

--- client2/src/components/LeftPane.js
+++ client2/src/components/LeftPane.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card } from '@material-ui/core';
 import Graph from "./Graph.js";
 
+//Sidebar containing graph for selected ROI(S)
 const LeftPane = () => {
 return (
     
